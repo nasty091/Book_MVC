7a4fac16988ff495942b57c245174e15906c17be
nasty091 <6151071035@st.utc2.edu.vn>
2023-09-04 22:15:22 +0700

    test push 2

--- BookWeb/Controllers/HomeController.cs
+++ BookWeb/Controllers/HomeController.cs
@@ -29,9 +29,5 @@ namespace BookWeb.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        public void Error(Exception error)
-        {
-
-        }
     }
 }
