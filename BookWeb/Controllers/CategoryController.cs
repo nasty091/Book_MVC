000adef5705e32c8c3e22fd93bad55f1e31be493
nasty091 <6151071035@st.utc2.edu.vn>
2023-10-23 21:12:47 +0700

    Create Category UI

---
+++ BookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using BookWeb.Data;
+using BookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db; 
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
