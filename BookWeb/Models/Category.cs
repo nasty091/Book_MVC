5892df0eb3fc90cd2a9640c2892bb873f4c78649
nasty091 <6151071035@st.utc2.edu.vn>
2023-10-24 16:28:56 +0700

    Server Side Validations

--- BookWeb/Models/Category.cs
+++ BookWeb/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace BookWeb.Models
 {
@@ -8,9 +9,11 @@ namespace BookWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(50)]
         [DisplayName("Category Name")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage = "The field Display Order must be between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
