02ba762f356dc9a88dc11f4279247392c206eb5c
nasty091 <6151071035@st.utc2.edu.vn>
2023-11-01 03:51:14 +0700

    Display all Categories

--- BookWebRazor_Temp/Pages/Categories/Index.cshtml.cs
+++ BookWebRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using BookWebRazor_Temp.Data;
+using BookWebRazor_Temp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,18 @@ namespace BookWebRazor_Temp.Pages.Categories
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+
+        public List<Category> CategoryList { get; set; }
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+            CategoryList = _db.Categories.ToList();
         }
     }
 }
