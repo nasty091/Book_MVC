7abc95e7874fbc196382816d1f7502c9bf6f132b
nasty091 <6151071035@st.utc2.edu.vn>
2023-11-19 13:30:52 +0700

    Replace DbContext with Category Repository

--- BookWeb/Controllers/CategoryController.cs
+++ BookWeb/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Book.DataAccess.Data;
+using Book.DataAccess.Repository.IRepository;
 using Book.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,14 +7,14 @@ namespace BookWeb.Controllers
 {
     public class CategoryController : Controller
     {
-        private readonly ApplicationDbContext _db; 
-        public CategoryController(ApplicationDbContext db)
+        private readonly ICategoryRepository _categoryRepo; 
+        public CategoryController(ICategoryRepository db)
         {
-            _db = db;
+            _categoryRepo = db;
         }
         public IActionResult Index()
         {
-            List<Category> objCategoryList = _db.Categories.ToList();
+            List<Category> objCategoryList = _categoryRepo.GetAll().ToList();
 
             return View(objCategoryList);
         }
@@ -32,8 +33,8 @@ namespace BookWeb.Controllers
             }
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(obj);
-                _db.SaveChanges();
+                _categoryRepo.Add(obj);
+                _categoryRepo.Save();
                 TempData["success"] = "Category created successfully"; //TempData: create notification //"success" is key name
                 return RedirectToAction("Index");
             }
@@ -47,7 +48,7 @@ namespace BookWeb.Controllers
                 return NotFound();
             }
 
-            Category categoryFromDb = _db.Categories.Find(id); //Find() just for Primary Key
+            Category categoryFromDb = _categoryRepo.Get(u => u.Id == id); //Find() just for Primary Keys
             //Category categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Name == "Hello");
             //Category categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault(); 
 
@@ -63,8 +64,8 @@ namespace BookWeb.Controllers
         {
             if (ModelState.IsValid)
             {
-                _db.Categories.Update(obj);
-                _db.SaveChanges();
+                _categoryRepo.Update(obj);
+                _categoryRepo.Save();
                 TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
@@ -78,7 +79,7 @@ namespace BookWeb.Controllers
                 return NotFound();
             }
 
-            Category categoryFromDb = _db.Categories.Find(id); //Find() just for Primary Key
+            Category categoryFromDb = _categoryRepo.Get(u => u.Id == id); //Find() just for Primary Key
 
             if (categoryFromDb == null)
             {
@@ -90,13 +91,13 @@ namespace BookWeb.Controllers
         [HttpPost, ActionName("Delete")]
         public IActionResult DeletePOST(int? id)
         {
-            Category obj = _db.Categories.Find(id);
+            Category obj = _categoryRepo.Get(u => u.Id == id); ;
             if (obj == null)
             {
                 return NotFound();
             }
-            _db.Categories.Remove(obj);
-            _db.SaveChanges();
+            _categoryRepo.Remove(obj);
+            _categoryRepo.Save();
             TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
         }
