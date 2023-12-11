acaa827b7b7caf9cc8d27439237de878b7a3133f
nasty091 <6151071035@st.utc2.edu.vn>
2023-12-11 21:57:28 +0700

    Fix Issue with Add to Cart

--- BookWeb/Areas/Customer/Controllers/HomeController.cs
+++ BookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -50,11 +50,24 @@ namespace BookWeb.Areas.Customer.Controllers
         [Authorize]
         public IActionResult Details(ShoppingCart shoppingCart)
         {
+            //Get User Id
             var claimsIdentity = (ClaimsIdentity)User.Identity;
             var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
             shoppingCart.ApplicationUserId = userId;
 
-            _unitOfWork.ShoppingCart.Add(shoppingCart);
+            ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.ApplicationUserId == userId && u.ProductId == shoppingCart.ProductId);
+            if(cartFromDb != null)
+            {
+                //Shopping cart exists
+                cartFromDb.Count += shoppingCart.Count;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);//ko can cung dc
+            }
+            else
+            {
+                //Add new card record
+                _unitOfWork.ShoppingCart.Add(shoppingCart);
+            }
+
             _unitOfWork.Save();
 
             return RedirectToAction(nameof(Index));
