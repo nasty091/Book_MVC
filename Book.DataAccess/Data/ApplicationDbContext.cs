440d6b000863432cc2cca0813478eb8a235ca85e
nasty091 <6151071035@st.utc2.edu.vn>
2023-12-14 21:42:53 +0700

    Create Order Header and Details Model

--- Book.DataAccess/Data/ApplicationDbContext.cs
+++ Book.DataAccess/Data/ApplicationDbContext.cs
@@ -18,6 +18,8 @@ namespace Book.DataAccess.Data
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Company> Companies { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
 
         //To seed(create) data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
