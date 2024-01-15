6e0a2a4502a2acd04e168d1d85093028426e3161
nasty091 <6151071035@st.utc2.edu.vn>
2024-01-15 15:55:45 +0700

    Azure Production Deployment

--- BookWeb/Areas/Admin/Controllers/OrderController.cs
+++ BookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -94,7 +94,7 @@ namespace BookWeb.Areas.Admin.Controllers
             orderHeader.ShippingDate = DateTime.Now;
             if(orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
             {
-                orderHeader.PaymentDueDate = DateOnly.FromDateTime(DateTime.Now.AddDays(30));
+                orderHeader.PaymentDueDate = DateTime.Now.AddDays(30);
             }
 
             _unitOfWork.OrderHeader.Update(orderHeader);
